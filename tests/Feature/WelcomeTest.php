b4dbd36f5ce9d2fef05541a9be705b327b578197
Muhammad Muneeb <itsmuneebrehman.conact@gmail.com>
2026-07-14 16:36:59 +0500

    feat: update welcome page test to include TV display link

--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -1,9 +1,11 @@
 <?php
 
-test('welcome page shows token display link', function () {
+test('welcome page shows token display and tv display links', function () {
     $response = $this->get(route('home'));
 
     $response->assertOk();
     $response->assertSee(route('display.tokens'));
     $response->assertSee('Token Display');
+    $response->assertSee(route('display.tokens.tv'));
+    $response->assertSee('TV Display');
 });
